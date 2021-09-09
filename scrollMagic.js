685b5004b72421ddb698b483f102f4ee8a49c537
James Rosas <jamesadrian777@gmail.com>
2021-09-09 16:39:48 -0500

    se implemento la animacion de scroll en el modo mobile

--- scrollMagic.js
+++ scrollMagic.js
@@ -11,9 +11,6 @@ const scrollFunction = (trigger, time,  element, animation) => {
         .addTo(controller);
 }
 
-// para manejar animaciones segun el viewport es mejor manipular los margin y posicion de los objetos desde el css para acercarlos o alejarlos para que encaje la naimacion, ya que manejando las animaciones por media queires atraves de JS las animaciones no fluyen bien
-
-// la animaciones en modo mobile como el viewport es mas alto entonces las animaciones ya se ha ejecutado antes de tiempo, por tanto en mobile las animaciones solo podrian disparase despues del above the fold en su version mobile, para que asi halla espacio para hacer srcoll
 
 const lightBall = {width: "1rem", transform: "translateY(245px)"};
 scrollFunction(".the-profession", 240, ".round-light", lightBall);
@@ -25,11 +22,20 @@ scrollFunction(".about-me_photo", 20, ".photo-box", lightPhoto2);
 const showPhoto = {opacity: "1", zIndex: "12"};
 scrollFunction(".about-me_photo", 20, ".photo-box img", showPhoto);
 
-const lightBallMobile = {width: "1rem", transform: "translateY(310px)"};
-scrollFunction(".light-container_mobile", 460, ".round-light_mobile", lightBallMobile);
-
 const moveLeftArm = {transform: "translateX(-300px)", opacity: "0"};
 scrollFunction(".the-profession", 200, ".left-arm", moveLeftArm);
 
 const moveRightArm = {transform: "translateX(300px)", opacity: "0"};
 scrollFunction(".the-profession", 200, ".right-arm", moveRightArm);
+
+// mobile version from here ******************************************
+
+const lightBallMobile = {width: "1rem", transform: "translateY(320px)"};
+scrollFunction(".trigger-mobile", 550, ".round-light_mobile", lightBallMobile);
+
+const moveLeftArmMobile = {transform: "translateX(-300px)", opacity: "0"};
+scrollFunction(".trigger-mobile", 500, ".left-arm_mobile", moveLeftArmMobile);
+
+const moveRightArmMobile = {transform: "translateX(300px)", opacity: "0"};
+scrollFunction(".trigger-mobile", 500, ".right-arm_mobile", moveRightArmMobile);
+
