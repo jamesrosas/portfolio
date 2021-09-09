ea30e043c5f6ad4a044ae2eba2f61c7adc19de7c
James Rosas <jamesadrian777@gmail.com>
2021-09-09 10:37:16 -0500

    implemetacion de animacion con scroll completada con las manos de la creacion de adan que apuntan a la .round-light y desaparecen con el scroll, solo resta la version mobile de esta

--- scrollMagic.js
+++ scrollMagic.js
@@ -15,15 +15,21 @@ const scrollFunction = (trigger, time,  element, animation) => {
 
 // la animaciones en modo mobile como el viewport es mas alto entonces las animaciones ya se ha ejecutado antes de tiempo, por tanto en mobile las animaciones solo podrian disparase despues del above the fold en su version mobile, para que asi halla espacio para hacer srcoll
 
-const lightBall = {width: "1rem", transform: "translateY(210px)"};
-scrollFunction(".the-profession", 200, ".round-light", lightBall);
+const lightBall = {width: "1rem", transform: "translateY(245px)"};
+scrollFunction(".the-profession", 240, ".round-light", lightBall);
 
 const lightPhoto = {animation: "electric 5s infinite ease-out"};
 const lightPhoto2 = {boxShadow: "0 0 12px cyan, 0 0 24px cyan", border: "2px solid white"};
-scrollFunction(".about-me", 20, ".photo-box", lightPhoto2);
+scrollFunction(".about-me_photo", 20, ".photo-box", lightPhoto2);
 
 const showPhoto = {opacity: "1", zIndex: "12"};
 scrollFunction(".about-me_photo", 20, ".photo-box img", showPhoto);
 
 const lightBallMobile = {width: "1rem", transform: "translateY(310px)"};
 scrollFunction(".light-container_mobile", 460, ".round-light_mobile", lightBallMobile);
+
+const moveLeftArm = {transform: "translateX(-300px)", opacity: "0"};
+scrollFunction(".the-profession", 200, ".left-arm", moveLeftArm);
+
+const moveRightArm = {transform: "translateX(300px)", opacity: "0"};
+scrollFunction(".the-profession", 200, ".right-arm", moveRightArm);
