c0b8a0b86f545c778b23cd890b41408baca753f2
James Rosas <jamesadrian777@gmail.com>
2021-10-13 10:26:04 -0500

    adaptacion de los trigger de scroll animation para un viewport de desktop. Tambien se le dio un ancho maximo al main para que los elementos no se desordenaran en un desktop-viewport

--- scrollMagic.js
+++ scrollMagic.js
@@ -16,23 +16,23 @@ scrollFunction(".glass-trigger", 240, ".fragment", fragmentMove);
 
 
 const lightBall = {width: "1rem", transform: "translateY(245px)"};
-scrollFunction(".the-profession", 240, ".round-light", lightBall);
+scrollFunction(".glass-trigger", 240, ".round-light", lightBall);
 
 const lightPhoto = {animation: "electric 5s infinite ease-out"};
 const lightPhoto2 = {boxShadow: "0 0 12px cyan, 0 0 24px cyan", border: "2px solid white"};
-scrollFunction(".about-me_photo", 20, ".photo-box", lightPhoto2);
+scrollFunction(".light-photo_trigger", 20, ".photo-box", lightPhoto2);
 
 const showPhoto = {opacity: "1", zIndex: "12"};
-scrollFunction(".about-me_photo", 20, ".photo-box img", showPhoto);
+scrollFunction(".light-photo_trigger", 20, ".photo-box img", showPhoto);
 
 const moveLeftArm = {transform: "translateX(-300px)", opacity: "0"};
-scrollFunction(".the-profession", 200, ".left-arm", moveLeftArm);
+scrollFunction(".glass-trigger", 200, ".left-arm", moveLeftArm);
 
 const moveRightArm = {transform: "translateX(300px)", opacity: "0"};
-scrollFunction(".the-profession", 200, ".right-arm", moveRightArm);
+scrollFunction(".glass-trigger", 200, ".right-arm", moveRightArm);
 
 const showSocialNetworks = {opacity: "1"};
-scrollFunction(".about-me_photo", 20, ".rrss svg", showSocialNetworks);
+scrollFunction(".light-photo_trigger", 20, ".rrss svg", showSocialNetworks);
 
 const skillsBar = { width: "100%"};
 scrollFunction(".skills-container", 280, ".animate-bar", skillsBar);
