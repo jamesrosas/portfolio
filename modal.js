a4a313bb8b0165ac2adc1e73e42aaa01a3d0953e
James Rosas <jamesadrian777@gmail.com>
2022-02-16 17:02:53 -0500

    adaptacion mobile de links -visitar sitio- para cada proyecto, para que se habiliten al activar los modales por medio de un click

--- modal.js
+++ modal.js
@@ -1,26 +1,31 @@
-const modalAction = (trigger, modal, close) => {
+const modalAction = (trigger, modal, close, visitLink) => {
     
     trigger.addEventListener('click', () => {
-    modal.classList.add('show-modal');
+        modal.classList.add('show-modal');
+        visitLink.classList.add('show-link')
     })
 
     close.addEventListener('click', () => {
         modal.classList.remove('show-modal');
+        visitLink.classList.remove('show-link')
     })
+
 }
 
 const PREVIEW = document.querySelector('.preview-text');
 const MODAL_DETAILS = document.querySelector('.preview-modal');
 const CLOSE_ICON = document.querySelector('.close-icon');
+const VISIT_BLACKSHEEP = document.querySelector('.visit-link_blacksheep')
 
-modalAction(PREVIEW, MODAL_DETAILS, CLOSE_ICON);
+modalAction(PREVIEW, MODAL_DETAILS, CLOSE_ICON, VISIT_BLACKSHEEP);
 
 // second modal
 const PREVIEW2 = document.querySelector('.preview-text2');
 const MODAL_DETAILS2 = document.querySelector('.preview-modal2');
 const CLOSE_ICON2 = document.querySelector('.close-icon2');
+const VISIT_SOCIALNET = document.querySelector('.visit-link_socialnet')
 
-modalAction(PREVIEW2, MODAL_DETAILS2, CLOSE_ICON2);
+modalAction(PREVIEW2, MODAL_DETAILS2, CLOSE_ICON2, VISIT_SOCIALNET);
 
 
 
